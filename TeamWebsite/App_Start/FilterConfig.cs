c56e97f64874848c1106c48cca876f4539ec2aef
LovinK <lovinkuruvilla@yahoo.com>
2018-09-03 11:54:50 -0500

    Basic pages created
    Created basic pages, must add more details and finish up other pages as well.

---
+++ TeamWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TeamWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TeamWebsite/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mentors()
+        {
+            ViewBag.Message = "Mentor page.";
+
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            ViewBag.Message = "Team page.";
+
+            return View();
+        }
+        
+        public ActionResult Meetings()
+        {
+            ViewBag.Message = "Meeting page";
+            return View();
+        }
+
+        public ActionResult Milestones()
+        {
+            ViewBag.Message = "Milestones achieved";
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            ViewBag.Message = "UHCL/project vidoes";
+            return View();
+        }
+
+
+    }
+}
